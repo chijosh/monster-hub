06dddf4ba2a285cec79148f08d3c05c02a40fc30
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-08-09 14:00:01 +0300

    Updates

---
+++ precache-manifest.4c533bea73fea3076a5dfade0a01222e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7707bc8de7fdfb5fe3c0f371bcbb38a1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a8b0ec56758577c11432",
+    "url": "/monster-rolodex/static/css/main.b808fba8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a8b0ec56758577c11432",
+    "url": "/monster-rolodex/static/js/main.b7498afc.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
